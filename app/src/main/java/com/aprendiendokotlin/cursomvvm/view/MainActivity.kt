cef7b553e81feb82b8b80f750fe9472d1badb608
BuildTools <unconfigured@null.spigotmc.org>
2024-01-27 13:26:30 -0600

    add uptate

---
+++ app/src/main/java/com/aprendiendokotlin/cursomvvm/view/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.aprendiendokotlin.cursomvvm.view
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.aprendiendokotlin.cursomvvm.databinding.ActivityMainBinding
+import com.aprendiendokotlin.cursomvvm.viewmodel.QuoteViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val quoteViewModel: QuoteViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        quoteViewModel.quoteModel.observe(this, Observer { currentQuote ->
+            binding.tvAuthor.text = currentQuote.author
+            binding.tvQuote.text = currentQuote.quote
+        })
+
+        binding.viewContainer.setOnClickListener {
+            quoteViewModel.randomQuote()
+        }
+    }
+}
--- app/src/main/java/com/aprendiendokotlin/cursomvvm/viewmodel/QuoteViewModel.kt
+++ app/src/main/java/com/aprendiendokotlin/cursomvvm/viewmodel/QuoteViewModel.kt
@@ -1,4 +1,16 @@
 package com.aprendiendokotlin.cursomvvm.viewmodel
 
-class QuoteViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.aprendiendokotlin.cursomvvm.model.QuoteModel
+import com.aprendiendokotlin.cursomvvm.model.QuoteProvider
+
+class QuoteViewModel: ViewModel() {
+
+    val quoteModel = MutableLiveData<QuoteModel>()
+
+    fun randomQuote(){
+        val currentQuote = QuoteProvider.random()
+        quoteModel.postValue(currentQuote)
+    }
 }
